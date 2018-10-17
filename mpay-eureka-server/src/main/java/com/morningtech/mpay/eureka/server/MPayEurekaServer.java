aca385e6b3ad22fbbf3ac8b92401993e855d3c77
mark <mark.ma@morningtech.com>
2018-10-17 15:14:52 +0800

    add spring-starter configuration

---
+++ mpay-eureka-server/src/main/java/com/morningtech/mpay/eureka/server/MPayEurekaServer.java
@@ -0,0 +1,17 @@
+package com.morningtech.mpay.eureka.server;
+
+/**
+ * @program: MPay-Cloud
+ * @description: Eureka server
+ * @author: Mark Ma
+ * @create: 2018-10-17 11:42
+ **/
+
+
+public class MPayEurekaServer {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+}
